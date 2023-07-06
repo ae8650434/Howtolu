d35dca62c461a5b92617bd72f2f3dd34067dafe3
seed707 <seed0102@gmail.com>
2023-07-06 11:22:41 +0800

    123

---
+++ src/components/product.jsx
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                
+               
+   
+
+
+    <div id="container">
+        <div id="categories" class="categories">
+            <p>商品分類</p>
+            <ul class="categoriesUl">
+                <li><a href="https://www.google.com/">寢室帳篷</a></li>
+                <li><a href="">客廳帳&天幕</a></li>
+                <li><a href="">寢室用具</a></li>
+                <li><a href="">戶外用品</a></li>
+                <li><a href="">爐具</a></li>
+                <li><a href="">保鮮保冷</a></li>
+                <li><a href="">燈具</a></li>
+                <li><a href="">影音設備</a></li>
+                <li><a href="">保暖裝備</a></li>
+                <li><a href="">常用配件</a></li>
+            </ul>
+        </div>
+
+
+        <div class="productcotent">
+
+            <figure>
+                <img src="./public/image/dog01.jpeg"/>
+                <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                <p></p>
+                <figcaption>NT$1,980 - 3 日</figcaption>
+                <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+            </figure>
+            <figure>
+                <img src="./public/image/dog01.jpeg"/>
+                <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                <p></p>
+                <figcaption>NT$1,980 - 3 日</figcaption>
+                <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+            </figure>
+            <figure>
+                <img src="./public/image/dog01.jpeg"/>
+                <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                <p></p>
+                <figcaption>NT$1,980 - 3 日</figcaption>
+                <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+            </figure>
+
+        </div>
+        <div>
+
+
+            <div class="productcotent2">
+                <figure>
+                    <img src="./public/image/dog01.jpeg"/>
+                    <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                    <p></p>
+                    <figcaption>NT$1,980 - 3 日</figcaption>
+                    <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+                </figure>
+                <figure>
+                    <img src="./public/image/dog01.jpeg"/>
+                    <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                    <p></p>
+                    <figcaption>NT$1,980 - 3 日</figcaption>
+                    <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+                </figure>
+                <figure>
+                    <img src="./public/image/dog01.jpeg"/>
+                    <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
+                    <p></p>
+                    <figcaption>NT$1,980 - 3 日</figcaption>
+                    <a href="./equip_detail.html"><button class="btnq">立即預約</button></a>
+                </figure>
+            </div>
+
+
+
+
+        </div>
+    </div>
+
+    <div class="br1">
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <br/><br/><br/><br/><br/><br/><br/>
+
+
+
+    </div>
+
+
+
+
+
+
+    <div>
+
+        <div class="categories1">
+            <p>商品分類</p>
+            <ul class="categoriesUl1">
+                <li><a href="https://www.google.com/">寢室帳篷</a></li>
+                <li><a href="">客廳帳&天幕</a></li>
+                <li><a href="">寢室用具</a></li>
+                <li><a href="">戶外用品</a></li>
+                <li><a href="">爐具</a></li>
+                <li><a href="">保鮮保冷</a></li>
+                <li><a href="">燈具</a></li>
+                <li><a href="">影音設備</a></li>
+                <li><a href="">保暖裝備</a></li>
+                <li><a href="">常用配件</a></li>
+            </ul>
+        </div>
+    </div>
+    <br/><br/>
+    
+
+    <div class="br1">
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    </div>
+
+
+
+
+
+
+
+            
+            </React.Fragment >
+        );
+    }
+}
+
+export default Product;
