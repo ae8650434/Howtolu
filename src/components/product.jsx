269abb92b01f2887e366211d4e89bcdaa816aa3b
ae8650434 <ae8650434@gmail.com>
2023-07-06 14:43:16 +0800

    dsad

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -36,7 +36,7 @@ class Product extends Component {
                 <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
                 <p></p>
                 <figcaption>NT$1,980 - 3 日</figcaption>
-                <a href="./equip_detail.html"><button className="btnq">立即預約</button></a>
+                <a href="/equip_detail"><button className="btnq">立即預約</button></a>
             </figure>
             <figure>
                 <img src="./image/dog01.jpeg"/>
@@ -50,7 +50,7 @@ class Product extends Component {
                 <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
                 <p></p>
                 <figcaption>NT$1,980 - 3 日</figcaption>
-                <a href="./equip_detail.html"><button className="btnq">立即預約</button></a>
+                <a href="/equip_detail"><button className="btnq">立即預約</button></a>
             </figure>
 
         </div>
@@ -63,21 +63,21 @@ class Product extends Component {
                     <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
                     <p></p>
                     <figcaption>NT$1,980 - 3 日</figcaption>
-                    <a href="./equip_detail.html"><button className="btnq">立即預約</button></a>
+                    <a href="/equip_detail"><button className="btnq">立即預約</button></a>
                 </figure>
                 <figure>
                     <img src="./image/dog01.jpeg"/>
                     <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
                     <p></p>
                     <figcaption>NT$1,980 - 3 日</figcaption>
-                    <a href="./equip_detail.html"><button className="btnq">立即預約</button></a>
+                    <a href="/equip_detail"><button className="btnq">立即預約</button></a>
                 </figure>
                 <figure>
                     <img src="./image/dog01.jpeg"/>
                     <figcaption>比利時CanvasCamp鐘型帳(2~6人)</figcaption>
                     <p></p>
                     <figcaption>NT$1,980 - 3 日</figcaption>
-                    <a href="./equip_detail.html"><button className="btnq">立即預約</button></a>
+                    <a href="/equip_detail"><button className="btnq">立即預約</button></a>
                 </figure>
             </div>
 
