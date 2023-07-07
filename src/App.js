01fec6572608fd96cfadd4dce3f2c76e4aef01cf
ae8650434 <ae8650434@gmail.com>
2023-07-07 14:11:43 +0800

    dsajl;d

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Revise from './components/revise';
 import Reset from './components/reset';
 import Info from './components/info';
 import EquipDetail from './components/equip_detail';
-// import Cart from './components/cart';
+import Cart from './components/cart';
 import Order_list from './components/order_list';
  
 class App extends Component {
@@ -29,7 +29,7 @@ class App extends Component {
              <Route path="/info" component={Info} exact/>
              <Route path="/order_list" component={Order_list} exact/>
              <Route path="/equip_detail" component={EquipDetail} exact/>
-             {/* <Route path="/cart" component={Cart} exact/> */}
+             <Route path="/cart" component={Cart} exact/>
             <Route component={Error}/>
            </Switch>
         </div> 
