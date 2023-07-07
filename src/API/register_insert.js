1a41819b4e13051d1cc30198d1ad885676ebfa65
AngelWinnie <angel04110901@gmail.com>
2023-07-07 12:15:08 +0800

    1111

---
+++ src/API/register_insert.js
@@ -0,0 +1,39 @@
+// 連接資料庫
+var DB = require("../API/DB");
+var express = require("express");
+var app = express.Router();
+var bcrypt = require("bcrypt");
+// var password = 'a12345678'
+
+// console.log(password)
+
+app.post("/register_member", express.urlencoded(), (req, res) => {
+  var name = req.body.name;
+  var phone = req.body.phone;
+  var email = req.body.email;
+  var rowpassword = req.body.password;
+  var date = new Date();
+  // 密碼加密
+  const saltRounds = 10;
+  bcrypt.hash(rowpassword, saltRounds, (err, hash) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error during password hashing");
+      return;
+    }
+
+    var sql =
+      "INSERT INTO member(name, tel, password, mail, register_time) VALUES (?,?,?,?,?)";
+    DB.query(sql, [name, phone, hash, email, date], (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error during registration");
+        return;
+      }
+      
+      res.send('Registration successful');
+    });
+  });
+});
+
+module.exports = app;
--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/nav';
 import Home from './components/index';
 import Error from './components/error';
-import Product from './components/product';
+// import Product from './components/product';
 import Login from './components/login';
 import Register from './components/register';
 import Revise from './components/revise';
 import Reset from './components/reset';
 import Info from './components/info';
 import EquipDetail from './components/equip_detail';
-import Cart from './components/cart';
+// import Cart from './components/cart';
 import Order_list from './components/order_list';
  
 class App extends Component {
@@ -21,7 +21,7 @@ class App extends Component {
           <Navigation />
             <Switch>
             <Route path="/" component={Home} exact/>
-            <Route path="/product" component={Product} exact/>
+            {/* <Route path="/product" component={Product} exact/> */}
             <Route path="/login" component={Login} exact/>
              <Route path="/register" component={Register} exact/>
              <Route path="/revise" component={Revise} exact/>
@@ -29,7 +29,7 @@ class App extends Component {
              <Route path="/info" component={Info} exact/>
              <Route path="/order_list" component={Order_list} exact/>
              <Route path="/equip_detail" component={EquipDetail} exact/>
-             <Route path="/cart" component={Cart} exact/>
+             {/* <Route path="/cart" component={Cart} exact/> */}
             <Route component={Error}/>
            </Switch>
         </div> 
