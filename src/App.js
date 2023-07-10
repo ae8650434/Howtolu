c2f2550e9a8703194cba5332a3b6559fcef27b78
seed707 <seed0102@gmail.com>
2023-07-10 14:18:24 +0800

    1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/nav';
 import Home from './components/index';
 import Error from './components/error';
-// import Product from './components/product';
-// import Product2 from './components/product2';
+import Product from './components/product';
+import Product2 from './components/product2';
 import Login from './components/login';
 import Register from './components/register';
 import Revise from './components/revise';
@@ -14,31 +14,31 @@ import EquipDetail from './components/equip_detail';
 // import Cart from './components/cart';
 import Order_list from './components/order_list';
 
- 
+
 class App extends Component {
   render() {
-    return (      
-       <BrowserRouter>
+    return (
+      <BrowserRouter>
         <div>
           <Navigation />
-            <Switch>
-            <Route path="/" component={Home} exact/>
-            {/* <Route path="/product" component={Product} exact/> */}
-            {/* <Route path="/product2" component={Product2} exact/> */}
-            <Route path="/login" component={Login} exact/>
-             <Route path="/register" component={Register} exact/>
-             <Route path="/revise" component={Revise} exact/>
-             <Route path="/reset" component={Reset} exact/>
-             <Route path="/info" component={Info} exact/>
-             <Route path="/order_list" component={Order_list} exact/>
-             <Route path="/equip_detail" component={EquipDetail} exact/>
-             {/* <Route path="/cart" component={Cart} exact/> */}
-            <Route component={Error}/>
-           </Switch>
-        </div> 
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/product" component={Product} exact /> 
+            <Route path="/product2" component={Product2} exact /> 
+            <Route path="/login" component={Login} exact />
+            <Route path="/register" component={Register} exact />
+            <Route path="/revise" component={Revise} exact />
+            <Route path="/reset" component={Reset} exact />
+            <Route path="/info" component={Info} exact />
+            <Route path="/order_list" component={Order_list} exact />
+            <Route path="/equip_detail" component={EquipDetail} exact />
+            {/* <Route path="/cart" component={Cart} exact/> */}
+            <Route component={Error} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
 }
- 
+
 export default App;
