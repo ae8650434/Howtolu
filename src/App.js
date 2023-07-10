71814d67c33e5566f789c93232e5995ef24e0e22
seed707 <seed0102@gmail.com>
2023-07-10 14:22:31 +0800

    1

--- src/App.js
+++ src/App.js
@@ -17,17 +17,12 @@ import Order_list from './components/order_list';
 import Footer from './components/footer';
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/origin/main
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
           <Navigation />
-<<<<<<< HEAD
           <Switch>
             <Route path="/" component={Home} exact />
             <Route path="/product" component={Product} exact /> 
@@ -43,26 +38,6 @@ class App extends Component {
             <Route component={Error} />
           </Switch>
         </div>
-=======
-            <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path="/product" component={Product} exact/>
-            <Route path="/product2" component={Product2} exact/>
-            <Route path="/login" component={Login} exact/>
-             <Route path="/register" component={Register} exact/>
-             <Route path="/revise" component={Revise} exact/>
-             <Route path="/reset" component={Reset} exact/>
-             <Route path="/info" component={Info} exact/>
-             <Route path="/order_list" component={Order_list} exact/>
-             <Route path="/equip_detail" component={EquipDetail} exact/>
-             <Route path="/food_detail" component={FoodDetail} exact/>
-             <Route path="/cart" component={Cart} exact/>
-            <Route component={Error}/>
-           </Switch>
-           <Footer />
-        </div> 
-         
->>>>>>> refs/remotes/origin/main
       </BrowserRouter>
     );
   }
