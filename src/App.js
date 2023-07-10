48dc41c377bf93f3769839edcb754a8b57012118
ae8650434 <ae8650434@gmail.com>
2023-07-10 13:37:17 +0800

    354

--- src/App.js
+++ src/App.js
@@ -15,32 +15,32 @@ import Cart from './components/cart';
 import Order_list from './components/order_list';
 import Footer from './components/footer';
 
- 
+
 class App extends Component {
   render() {
-    return (      
-       <BrowserRouter>
+    return (
+      <BrowserRouter>
         <div>
           <Navigation />
-            <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path="/product" component={Product} exact/>
-            <Route path="/product2" component={Product2} exact/>
-            <Route path="/login" component={Login} exact/>
-             <Route path="/register" component={Register} exact/>
-             <Route path="/revise" component={Revise} exact/>
-             <Route path="/reset" component={Reset} exact/>
-             <Route path="/info" component={Info} exact/>
-             <Route path="/order_list" component={Order_list} exact/>
-             <Route path="/equip_detail" component={EquipDetail} exact/>
-             <Route path="/cart" component={Cart} exact/>
-            <Route component={Error}/>
-           </Switch>
-           <Footer />
-        </div> 
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/product" component={Product} exact />
+            <Route path="/product2" component={Product2} exact />
+            <Route path="/login" component={Login} exact />
+            <Route path="/register" component={Register} exact />
+            <Route path="/revise" component={Revise} exact />
+            <Route path="/reset" component={Reset} exact />
+            <Route path="/info" component={Info} exact />
+            <Route path="/order_list" component={Order_list} exact />
+            <Route path="/equip_detail" component={EquipDetail} exact />
+            <Route path="/cart" component={Cart} exact />
+            <Route component={Error} />
+          </Switch>
+          <Footer />
+        </div>
       </BrowserRouter>
     );
   }
 }
- 
+
 export default App;
