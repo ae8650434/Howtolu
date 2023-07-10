75c798950890cfe772e39704cafbf0e47e75924b
ae8650434 <ae8650434@gmail.com>
2023-07-10 09:41:06 +0800

    354

--- src/API/app.js
+++ src/API/app.js
@@ -1,8 +1,12 @@
 var express=require("express");
 var app= express()
 var DB = require("./DB.js");
-var select=require("./select")
+var selectall=require("./selectall.js")
+var select=require("./select.js")
+var register=require("./register_insert.js")
 app.use("/product",select)
+app.use("/product/list",selectall)
+app.use("/register",register)
 
 app.listen(8000,function(){
     console.log('啟動')
