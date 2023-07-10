c3e8f398b07b64c507f6d1b0f15c79a2415c7716
ae8650434 <ae8650434@gmail.com>
2023-07-10 13:23:16 +0800

    354

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/nav';
 import Home from './components/index';
 import Error from './components/error';
-// import Product from './components/product';
-// import Product2 from './components/product2';
+import Product from './components/product';
+import Product2 from './components/product2';
 import Login from './components/login';
 import Register from './components/register';
 import Revise from './components/revise';
 import Reset from './components/reset';
 import Info from './components/info';
 import EquipDetail from './components/equip_detail';
-// import Cart from './components/cart';
+import Cart from './components/cart';
 import Order_list from './components/order_list';
 import Footer from './components/footer';
 
@@ -24,8 +24,8 @@ class App extends Component {
           <Navigation />
             <Switch>
             <Route path="/" component={Home} exact/>
-            {/* <Route path="/product" component={Product} exact/> */}
-            {/* <Route path="/product2" component={Product2} exact/> */}
+            <Route path="/product" component={Product} exact/>
+            <Route path="/product2" component={Product2} exact/>
             <Route path="/login" component={Login} exact/>
              <Route path="/register" component={Register} exact/>
              <Route path="/revise" component={Revise} exact/>
@@ -33,7 +33,7 @@ class App extends Component {
              <Route path="/info" component={Info} exact/>
              <Route path="/order_list" component={Order_list} exact/>
              <Route path="/equip_detail" component={EquipDetail} exact/>
-             {/* <Route path="/cart" component={Cart} exact/> */}
+             <Route path="/cart" component={Cart} exact/>
             <Route component={Error}/>
            </Switch>
            <Footer />
