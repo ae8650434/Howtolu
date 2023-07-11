42e184c8c0c57b556f69b4b55980c06736fb8937
ae8650434 <ae8650434@gmail.com>
2023-07-11 09:26:53 +0800

    4153

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,23 +5,6 @@ import axios from 'axios'
 //import '../js/product.js'
 class Product extends Component {
     state = {
-<<<<<<< HEAD
-        productList: [{"pid":1,"pname":"鐘型帳(2-6人)","price":1800,"day":3,"reserve":20,"p_img":"produc_1.png","description":"純棉材質具有防水、透氣及抗紫外線的功能，適用於4季各種不同的氣候下使用;建議適用人數：2人(奢華)/4人(舒適)/最多6人(睡袋);建議搭帳人數：1人以上;適用場地 : 草皮區","pc_id":1,"information":"套裝內容 : 帳篷本體、本體營柱x1、A型門柱x1、本體營釘X13、側裙營樁X14、附調節片營繩 X14、本體攜行袋、營柱收納袋、營釘收納袋;隨附配件：營槌、防水地布(先鋪設再搭帳)、帳內地墊;面積：4.83坪;直徑：450cm;高度：274cm;門高：167cm;最大容量：6人;總重量: 36 kg"}]
-    }
-    render() {
-        return(
-            <div>
-
-            {  this.state.productList.map((x)=>{
-                return   (<figure>
-                    <img src={`/image/${x.p_img}`} />
-                    <figcaption>{x.pname}</figcaption>
-                    <p></p>
-                    <figcaption>NT$1,980 - 3 日</figcaption>
-                    <a href="/equip_detail"><button className="btnq">立即預約</button></a>
-                    </figure> )                                      
-                })}  
-=======
         productList: {
             p_img:[{"pid":1,"pname":"鐘型帳(2-6人)","price":1800,"day":3,"reserve":20,"image":"produc_1.png","description":"純棉材質具有防水、透氣及抗紫外線的功能，適用於4季各種不同的氣候下使用;建議適用人數：2人(奢華)/4人(舒適)/最多6人(睡袋);建議搭帳人數：1人以上;適用場地 : 草皮區","class":1,"information":"套裝內容 : 帳篷本體、本體營柱x1、A型門柱x1、本體營釘X13、側裙營樁X14、附調節片營繩 X14、本體攜行袋、營柱收納袋、營釘收納袋;隨附配件：營槌、防水地布(先鋪設再搭帳)、帳內地墊;面積：4.83坪;直徑：450cm;高度：274cm;門高：167cm;最大容量：6人;總重量: 36 kg"}],
         }
@@ -108,8 +91,8 @@ class Product extends Component {
 
 
                     </div>
->>>>>>> f2e92c003f38c8b070194fa40a4ed838719c5983
                 </div>
+                </React.Fragment>
             );
         }
 componentDidMount = async () => {
