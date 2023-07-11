4e37bd60a0650a6003b137d08bc1f47b8728adb9
YA YI CHANG <molly10121110@gmail.com>
2023-07-11 14:01:53 +0800

    123

--- src/App.js
+++ src/App.js
@@ -39,17 +39,6 @@ class App extends Component {
             <Route path="/equip_detail/:pid" component={EquipDetail} exact />
             <Route path="/food_detail/:fid" component={FoodDetail} exact />
             <Route path="/cart" component={Cart} exact />
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-
->>>>>>> f914be1e5e7dd482e6d3b5fa95780a379e686000
->>>>>>> refs/remotes/origin/main
->>>>>>> 20fe92e51856fcfd5890ba42baffca4edf377243
             <Route component={Error} />
           </Switch>
           <Footer />
