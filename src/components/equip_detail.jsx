26f9e4cd1be0002f0035b8c22ea73326b30a5cd6
ae8650434 <ae8650434@gmail.com>
2023-07-11 13:55:00 +0800

    4153

--- src/components/equip_detail.jsx
+++ src/components/equip_detail.jsx
@@ -8,11 +8,11 @@ class EquipDetail extends Component {
     states = {
 
         product:
-        {
+        [{
             pid: 1, pname: "鐘型帳(2-6人)", price: 1800, day: 3, reserve: 20, p_img: "product_1.png",
             description: "純棉材質具有防水、透氣及抗紫外線的功能，適用於4季各種不同的氣候下使用;建議適用人數：2人(奢華)/4人(舒適)/最多6人(睡袋);建議搭帳人數：1人以上;適用場地 : 草皮區",
             pc_id: 1, information: "套裝內容 : 帳篷本體、本體營柱x1、A型門柱x1、本體營釘X13、側裙營樁X14、附調節片營繩 X14、本體攜行袋、營柱收納袋、營釘收納袋;隨附配件：營槌、防水地布(先鋪設再搭帳)、帳內地墊;面積：4.83坪;直徑：450cm;高度：274cm;門高：167cm;最大容量：6人;總重量: 36 kg"
-        }
+        }]
     }
     state = {
         product: 
@@ -118,7 +118,8 @@ class EquipDetail extends Component {
     // };
     render() {
         const { value, maxDate, minDate } = this.state;
-        const productArray = this.states.product.description.split(";");
+        {console.log(this.states.product[0])}
+        const productArray = this.states.product[0].description.split(";");
         return (
             <React.Fragment>
 
@@ -140,16 +141,16 @@ class EquipDetail extends Component {
 
                 <div>
                     <div className={styles.image}>
-                        <a target="_blank" href="/image/product_1.png">
-                            <img src="/image/product_1.png" /></a>
+                        <a target="_blank" href={`/image/${this.states.product[0].p_img}` }>
+                            <img src={`/image/${this.states.product[0].p_img}` }/></a>
                     </div>
                     <div className={styles.commodityall}>
                         <div className={styles.commodity}>
-                            <p>{this.states.product.pname}</p>
+                            <p>{this.states.product[0].pname}</p>
                             <p>
                                 <span>NT$</span>
-                                <span>{this.states.product.price}</span>
-                                <span>— {this.states.product.day}日</span>
+                                <span>{this.states.product[0].price}</span>
+                                <span>— {this.states.product[0].day}日</span>
                             </p>
                             <ul>
                                 {productArray.map((data, index) => (
@@ -277,8 +278,9 @@ class EquipDetail extends Component {
         var newStates = { ...this.states };
         newStates.product = result.data;
         // console.log("555",newState.product)
-        this.setState(newStates);
-
+        this.states=newStates;
+        this.setState(this.states);
+        
         // console.log("123",newState.product); //undefined
     }
 }
