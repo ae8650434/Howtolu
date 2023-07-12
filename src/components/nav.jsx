34cb658b8d19d1d910f1083d0404697075d504ee
eros758605 <eros758605@gmail.com>
2023-07-12 09:20:01 +0800

    9453

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -35,7 +35,7 @@ class  Navigation extends Component{
                             <a href=""><li className={navstyle.conli}>表單上傳</li></a>  
                             <li  className={navstyle.conli}>表單填寫
                                 <ul id={navstyle['a']}>
-                                    <a href=""><li >Google表單</li></a>  
+                                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfBpC90D4LAPmwi5iGMOShmuA6D1Lj0IvsvNUr7HUmzzhWzrQ/viewform"><li >Google表單</li></a>  
                                     <a href="/excel/HowTo露(表單回覆).xlsx" download><li >Excel表單下載</li></a>  
                                     
                                 </ul>
