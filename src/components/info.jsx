4ecfef5fb7d8c1531548e4492d320a21e73c91bb
YA YI CHANG <molly10121110@gmail.com>
2023-07-14 15:35:46 +0800

    22222

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -2,74 +2,74 @@ import React, { Component } from 'react';
 import styles from '../css/Info_member.module.css'
 
 class Info extends Component {
-    state = {  } 
-    render() { 
+    state = {}
+    render() {
         return (
 
             <div className={styles.short}>
-            <ul className={styles.info_ul}>
-                <li className={styles.info_li}><a href="/info" style={{textDecoration: "none"}}><p className={styles.info_li_first}>編輯會員資料</p></a></li>
-                <li className={styles.info_li}><a href="/order_list" style={{textDecoration: "none"}}><p className={styles.form}>訂單查詢</p></a></li>
-                <li className={styles.info_li}>
-                <a href="" style={{textDecoration: "none"}}>登出</a>
-                </li>
-            </ul>
+                <ul className={styles.info_ul}>
+                    <li className={styles.info_li}><a href="/info" style={{ textDecoration: "none" }}><p className={styles.info_li_first}>編輯會員資料</p></a></li>
+                    <li className={styles.info_li}><a href="/order_list" style={{ textDecoration: "none" }}><p className={styles.form}>訂單查詢</p></a></li>
+                    <li className={styles.info_li}>
+                        <a href="" style={{ textDecoration: "none" }}>登出</a>
+                    </li>
+                </ul>
 
-            <form action="" className={styles.form}>
-                <p className={styles.info_p_first}>我的大頭照</p>
-                <img src="/image/Head.png" className={styles.info_pic}/>
-                <input type="file" className={styles.info_file} />
-                <p className={styles.info_file_limit}>*請傳.jpg檔</p>
-            <div style={{textAlign: "center"}} className={styles.form}>
-                <div className={styles.info_from_div}>
-                    <label for="" className={styles.info_label_word}>姓 名</label>
-                    <input type="text" className={styles.info_label} />
-                    <label for="" className={styles.info_label_word}>手機號碼</label>
-                    <input type="tel" className={styles.info_label} />
-                </div>
-                <div className={styles.info_from_div}>
-                    <label for="" className={styles.info_label_word}>性 別 </label>
-                    <select name="" id="" className={styles.info_select}>
-                        <option>請選擇</option>
-                        <option>男性</option>
-                        <option>女性</option>
-                        <option>其他</option>
-                    </select>
-                    <label for="" className={styles.info_label_word}>e-mail</label>
-                    <input type="email" className={styles.info_label} />
-                </div>
-                <div className={styles.info_from_div}>
-                    <label for="" className={styles.info_label_word}>居住地址（選填）</label>
-                    <input type="text" className={styles.info_label_address} />
-                </div>
-                <div className={styles.info_reset_password}>
-                    <div className={styles.info_password_div}>
-                    <img src="/image/member_icon.png" className={styles.info_member_icon} />
-                    <p className={styles.info_pic_word}>修改密碼</p>
+                <form action="" className={styles.form}>
+                    <p className={styles.info_p_first}>我的大頭照</p>
+                    <img src="/image/Head.png" className={styles.info_pic} />
+                    <input type="file" className={styles.info_file} />
+                    <p className={styles.info_file_limit}>*請傳.jpg檔</p>
+                    <div style={{ textAlign: "center" }} className={styles.form}>
+                        <div className={styles.info_from_div}>
+                            <label for="" className={styles.info_label_word}>姓 名</label>
+                            <input type="text" className={styles.info_label} />
+                            <label for="" className={styles.info_label_word}>手機號碼</label>
+                            <input type="tel" className={styles.info_label} />
+                        </div>
+                        <div className={styles.info_from_div}>
+                            <label for="" className={styles.info_label_word}>性 別 </label>
+                            <select name="" id="" className={styles.info_select}>
+                                <option>請選擇</option>
+                                <option>男性</option>
+                                <option>女性</option>
+                                <option>其他</option>
+                            </select>
+                            <label for="" className={styles.info_label_word}>e-mail</label>
+                            <input type="email" className={styles.info_label} />
+                        </div>
+                        <div className={styles.info_from_div}>
+                            <label for="" className={styles.info_label_word}>居住地址（選填）</label>
+                            <input type="text" className={styles.info_label_address} />
+                        </div>
+                        <div className={styles.info_reset_password}>
+                            <div className={styles.info_password_div}>
+                                <img src="/image/member_icon.png" className={styles.info_member_icon} />
+                                <p className={styles.info_pic_word}>修改密碼</p>
+                            </div>
+                            <div className={styles.info_password}>
+                                <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>舊密碼</label>
+                                <input type="text" className={styles.info_label} />
+                            </div>
+                            <div className={styles.info_password}>
+                                <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>設定新密碼</label>
+                                <input type="text" className={styles.info_label} />
+                            </div>
+                            <div className={styles.info_password}>
+                                <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>確認新密碼</label>
+                                <input type="text" className={styles.info_label} />
+                            </div>
+                            <br /><br />
+                            <div className={styles.info_submit_div}>
+                                <input type="submit" value="儲存變更" className={styles.info_submit} />
+                            </div>
+                        </div>
                     </div>
-                    <div className={styles.info_password}>
-                        <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>舊密碼</label>
-                        <input type="text" className={styles.info_label} />
-                    </div>
-                    <div className={styles.info_password}>
-                        <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>設定新密碼</label>
-                        <input type="text" className={styles.info_label} />
-                    </div>
-                    <div className={styles.info_password}>
-                        <label for="" className={`${styles.info_label_word} ${styles.info_password_word}`}>確認新密碼</label>
-                        <input type="text" className={styles.info_label} />
-                    </div>
-                    <br/><br/>
-                    <div className={styles.info_submit_div}>
-                        <input type="submit" value="儲存變更" className={styles.info_submit} />
-                    </div>
-                </div>
+                </form>
+                <br /><br /><br /><br />
             </div>
-            </form>
-            <br /><br /><br /><br />
-        </div>
         );
     }
 }
- 
+
 export default Info;
--- src/components/order_list.jsx
+++ src/components/order_list.jsx
@@ -2,51 +2,51 @@ import React, { Component } from 'react';
 import styles from '../css/Order_list.module.css'
 
 class Order_list extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          displayOrderList: false
-        };
-      }
-    render() { 
-        const { displayOrderList } = this.state;
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayOrderList: false
+    };
+  }
+  render() {
+    const { displayOrderList } = this.state;
 
-        return (
-            <div className={styles.short}>
-                <ul className={styles.info_ul}>
-                    <li className={styles.info_li}><a href="/info" style={{textDecoration: "none"}}><p className="form">編輯會員資料</p></a></li>
-                    <li className={styles.info_li}><a href="/order_list" style={{textDecoration: "none"}}><p className="info_li_order form">訂單查詢</p></a></li>
-                    <li className={styles.info_li}>
-                    <a href="" style={{textDecoration: "none"}} className={styles.form}>登出</a>
-                    </li>
-                </ul>
+    return (
+      <div className={styles.short}>
+        <ul className={styles.info_ul}>
+          <li className={styles.info_li}><a href="/info" style={{ textDecoration: "none" }}><p className="form">編輯會員資料</p></a></li>
+          <li className={styles.info_li}><a href="/order_list" style={{ textDecoration: "none" }}><p className={styles.info_li_order}>訂單查詢</p></a></li>
+          <li className={styles.info_li}>
+            <a href="" style={{ textDecoration: "none" }} className={styles.form}>登出</a>
+          </li>
+        </ul>
 
-      <table className={styles.info_table}>
-        <tr>
-          <th className={styles.info_th}>訂單編號</th>
-          <th className={styles.info_th}>訂單日期</th>
-          <th className={styles.info_th}>合計</th>
-          <th className={styles.info_th}>租借日</th>
-          <th className={styles.info_th}>歸還日</th>
-          <th className={styles.info_th}>結案</th>
-          <th className={styles.info_th}></th>
-        </tr>
-        <tr>
-          <td rowspan={3} className={styles.info_td}>20230602123</td>
-          <td rowspan={3} className={styles.info_td}>2023/06/02</td>
-          <td rowspan={3} className={styles.info_td}>NT$3,000</td>
-          <td rowspan={3} className={styles.info_td}>2023/05/30</td>
-          <td rowspan={3} className={styles.info_td}>2023/06/01</td>
-          <td rowspan={3} className={styles.info_td}>未歸還</td>
-          <td><input type="button" value="查看明細" className={styles.info_order_button} id="checklist" onClick={this.handleChecklistClick}/></td>
-        </tr>
-        <tr>
-          <td><input type="button" value="再次購買" className={styles.info_order_button}/></td>
-        </tr>
-        
-        <tr>
-          <td><input type="button" value="訂單下載" className={`${styles.info_order_button} ${styles.info_last_buttom}`}/></td>
-        </tr>
+        <table className={styles.info_table}>
+          <tr>
+            <th className={styles.info_th}>訂單編號</th>
+            <th className={styles.info_th}>訂單日期</th>
+            <th className={styles.info_th}>合計</th>
+            <th className={styles.info_th}>租借日</th>
+            <th className={styles.info_th}>歸還日</th>
+            <th className={styles.info_th}>結案</th>
+            <th className={styles.info_th}></th>
+          </tr>
+          <tr>
+            <td rowspan={3} className={styles.info_td}>20230602123</td>
+            <td rowspan={3} className={styles.info_td}>2023/06/02</td>
+            <td rowspan={3} className={styles.info_td}>NT$3,000</td>
+            <td rowspan={3} className={styles.info_td}>2023/05/30</td>
+            <td rowspan={3} className={styles.info_td}>2023/06/01</td>
+            <td rowspan={3} className={styles.info_td}>未歸還</td>
+            <td><input type="button" value="查看明細" className={styles.info_order_button} id="checklist" onClick={this.handleChecklistClick} /></td>
+          </tr>
+          <tr>
+            <td><input type="button" value="再次購買" className={styles.info_order_button} /></td>
+          </tr>
+
+          <tr>
+            <td><input type="button" value="訂單下載" className={`${styles.info_order_button} ${styles.info_last_buttom}`} /></td>
+          </tr>
 
         </table>
 
@@ -69,7 +69,7 @@ class Order_list extends Component {
               <td className={styles.info_order_word}>0911123666</td>
             </tr>
             <tr>
-              <td colspan={5} className={styles.info_order_list_title}>訂購明細</td>
+              <td colspan={5} className={styles.info_order_list_title} style={{backgroundColor:"#D4C19F",color:"black"}}>訂購明細</td>
             </tr>
             <tr>
               <td className={`${styles.info_order_word} ${styles.info_order_word_bg}`}>項次</td>
@@ -80,23 +80,23 @@ class Order_list extends Component {
             </tr>
             <tr>
               <td className={`${styles.info_order_list_word} ${styles.info_order_num}`}>1</td>
-              <td className={styles.info_order_list_word} ><img src="/image/pimg.png" className={styles.info_p_img}/></td>
+              <td className={styles.info_order_list_word} ><img src="/image/pimg.png" className={styles.info_p_img} /></td>
               <td className={`${styles.info_order_list_word} ${styles.info_order_pname}`}>韓國甲珍雙人電熱毯(不指定花色)</td>
               <td className={`${styles.info_order_list_word} ${styles.info_order_num}`}>2</td>
               <td className={`${styles.info_order_list_word} ${styles.info_order_num}`}>NT$150</td>
             </tr>
           </table>
-          </div>
-          <br /><br /><br /><br />
-    </div>
-        );
-    }
+        </div>
+        <br /><br /><br /><br />
+      </div>
+    );
+  }
 
-    handleChecklistClick = () => {
-        this.setState(prevState => ({
-          displayOrderList: !prevState.displayOrderList
-        }));
-      }
+  handleChecklistClick = () => {
+    this.setState(prevState => ({
+      displayOrderList: !prevState.displayOrderList
+    }));
+  }
 }
- 
+
 export default Order_list;
