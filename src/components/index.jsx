9ff8ab48d2e4f93b5dde03fbe8c5989d2bd031ce
ae8650434 <ae8650434@gmail.com>
2023-07-16 21:30:54 +0800

    0122

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import About from './index/about.jsx'
 import Productlink from './index/productlink.jsx'
+import Activity from './index/activity.jsx'
+import Weather from './index/weatherapi.jsx'
+import Map from './index/map.jsx'
  
 const home = () => {
     return (
