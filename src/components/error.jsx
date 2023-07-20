5bd52a70380046a3e39e0ad2b49b4c995fa48ca6
ae8650434 <ae8650434@gmail.com>
2023-07-20 15:20:50 +0800

    dsad

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -4,7 +4,7 @@ import errstyle from "../css/error.module.css"
 const Error = () => {
     return (
        <div>
-          <p style={{fontSize:"50px" ,margin:"60px"}} className={errstyle.bounce}>沒有這頁啦 操 硬要點是不是</p>
+          <p style={{fontSize:"100px" ,margin:"60px"}} className={errstyle.bounce}>沒有這頁啦 操 硬要點是不是</p>
        </div>
     );
 }
