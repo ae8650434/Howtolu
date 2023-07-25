acfd5a10a251f8627f16b805f4fbd3fcf79ba635
ae8650434 <ae8650434@gmail.com>
2023-07-25 10:28:25 +0800

    顯示異常更新

--- src/components/index/weatherapi.jsx
+++ src/components/index/weatherapi.jsx
@@ -3,60 +3,95 @@ import axios from 'axios';
 import weastyle from '../../css/about.module.css'
 
 
-function Weather() {
-   
-    const [posts, setPosts] = React.useState([]);
-  
 
-  
-    React.useEffect(() => {
-        fetch('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314')
-           .then((response) => response.json())
-           .then((data) => {
-              console.log("這是",data.records.locations[0].location);
-             
-              
-              setPosts(data);
-              console.log("這是",posts);
-              
-           })
-           .catch((err) => {
-              console.log(err.message);
-           });
-     }, []);
-    //}, [count])
+class  Weather extends Component {
+    state = {
+        data:{
+            records:{
+                locations:[{
+                    location:[
+                        {locationName:"",
+                        
+                        weatherElement:[{},
+                            {time:[
+                                {elementValue:[{value:null}]
+                                }
+                            ]},
+                            {},{},{},{},
+                            {time:[
+                                {elementValue:[{value:null}]
+                                }
+                            ]
 
-    return (<div id="weath">
-    <select id="selectWt" className={weastyle.weasel} onchange="cityChange()">
+                            }
+
+                        ]
+                        
+                    }
+
+                    ]}
+                ]
+            }
+        }
+
+      } 
+    render() { 
         
+        return (<div id="weath">
+        <select id="selectWt" className={weastyle.weasel} onchange="cityChange()">
+            
+            {  
+            this.state.data.records.locations[0].location.map((x,index)=>{
+                console.log("44343",x.weatherElement[6].time[0] )
+             return   <option value={index}>{x.locationName}</option>
+            })}
+              </select>
+              
+        <div>
+            <table id={weastyle["weatb"]}>
+                <tr id={weastyle["thd"]}>
+                    <th>
+                    {  
+            this.state.data.records.locations[0].location[0].weatherElement[1].time.map((x,index)=>{
+                // console.log("44343",x.weatherElement[6].time[0].elementValue[0].value )
+                console.log("44343",x )
+          var a = x.startTime
+            
+                return   <th>{a }</th>
+            })}
+                    </th>
+    
+                </tr>
+                <tr id="tmort" className={weastyle.trwea}>
+                    <td rowspan="2">早上</td>
+    
+                </tr>
+                <tr id="tmorr">
+    
+    
+                </tr>
+                <tr id="taft">
+                    <td rowspan="2">下午</td>
+    
+                </tr>
+                <tr id="tafr">
+    
+    
+                </tr>
+            </table>
+        </div>
+    </div>);
+    }
+    componentDidMount=async ()=>{
+        var result = await axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314');
+        var newState = { ...this.state };
+        newState = result;
         
-          </select>
-
-    <div>
-        <table id={weastyle["weatb"]}>
-            <tr id={weastyle["thd"]}>
-                <th></th>
-
-            </tr>
-            <tr id="tmort" className={weastyle.trwea}>
-                <td rowspan="2">早上</td>
-
-            </tr>
-            <tr id="tmorr">
-
-
-            </tr>
-            <tr id="taft">
-                <td rowspan="2">下午</td>
-
-            </tr>
-            <tr id="tafr">
-
-
-            </tr>
-        </table>
-    </div>
-</div>);
-  }
-
+        this.state=newState;
+        this.setState(this.state);
+        console.log(this.state.data.records.locations[0].location
+            )
+    }
+}
+ 
 export default Weather;
