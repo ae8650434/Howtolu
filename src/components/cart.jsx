1147990eb8ca8be69286394cd8a4933cc15d28aa
eros758605 <eros758605@gmail.com>
2023-07-25 10:19:39 +0800

    '更新excel'

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -123,19 +123,20 @@ class Cart extends Component {
     render() {
         const total = this.calculateTotal();
         const { value, maxDate, minDate, datepicker, cartList, handleOpen } = this.state;
+        const CartItems = cartList.length > 0; 
         return (
             <React.Fragment>
 
                 <br /><br /><br /><br />
                 <Excel />
-                {cartList.length > 0 ? (<Process1 />) : null}
+                {CartItems && <Process1 /> }
                 <br /><br /><br /><br />
-                {cartList.length === 0 ? (<Nullcart />) : null}
+                {!CartItems && <Nullcart />}
 
                 <div id={cartstyle['shopping']}>
                     <div id={cartstyle['null']}>
-                        <CartProduct cartList={cartList} />
-                        {/* <CartFood cartList={cartList} updateProductQuantity={this.updateProductQuantity} /> */}
+                        <CartProduct />
+                        <CartFood  updateProductQuantity={this.updateProductQuantity} />
                     </div>
                 </div>
 
@@ -168,7 +169,7 @@ class Cart extends Component {
             </div><br /><br /><br /><br /><br /> */}
 
                 {/* 總計跟前往結帳 */}
-                {cartList.length > 0 ? (
+                {CartItems && (
                     <div id={cartstyle['shopping2']}>
                         <span style={{ fontSize: 40 }}>總計：NT{total}</span>
                         <button onClick={this.delAll} id={cartstyle['buy']}><a href=''><span style={{ color: 'white' }}>一鍵刪除</span></a></button>
@@ -176,7 +177,7 @@ class Cart extends Component {
                             <button id={cartstyle['buy']}><span>前往結帳</span></button>
                         </a>
                     </div>
-                ) : null}
+                )}
                 <br /><br />
 
                 {/* {handleOpen &&
