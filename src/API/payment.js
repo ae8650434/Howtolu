64fd0c42b3aaf9bebd3faff2720a2b36cfa17fd4
AngelWinnie <angel04110901@gmail.com>
2023-07-26 11:36:29 +0800

    修改修改payment邏輯

--- src/API/payment.js
+++ src/API/payment.js
@@ -4,7 +4,7 @@ var app = express.Router();
 var DB = require("../API/DB");
 const { select } = require("react-cookies");
 
-app.post('/', function (req, res) {
+app.post('/', async function (req, res) {
     var today = new Date();
     // console.log("1111",today.slice(0,10))
     var insql =
@@ -22,85 +22,78 @@ app.post('/', function (req, res) {
         req.body.neworder.os = 4
     }
     // 把結帳完成的資料 新增至資料庫order裡
-    DB.query(
-        insql, [req.body.neworder.mid,
-        req.body.neworder.order_number,
-        today,
-        req.body.neworder.use_date,
-        req.body.neworder.return_date,
-        req.body.neworder.price,
-        req.body.neworder.os,
-
-    ], (err, data) => {
-        console.log("你好", req.body.neworder.mid)
-        console.log("看一下7", err)
-        return res.status(200).json({ data: data });
-    })
-
-
-    console.log("看一下js", req.body)
-
-    // 更新資料庫order_number末3碼
-    DB.query(
-        sesql, [
-        req.body.neworder.order_number
-    ], (err, data) => {
-        // console.log("你好", req.body.mid)
-        // console.log("看一下5", (data[0].oid).toString().padStart(3, '0'))
-        // console.log("看一下6", data)
-        if (err) {
-            console.log("看一下7", err);
-        } else {
-            var ddd = req.body.neworder.order_number + (data[0].oid).toString().padStart(3, '0')
-            console.log("ddd", ddd)
+    try {
+        const insertOrderResult = await new Promise((resolve, reject) => {
             DB.query(
-                upsql, [
-                ddd,
-                data[0].oid
-            ], (err, datas) => {
-                // console.log("你好", req.body.mid)
-                if (err) {
-                    console.log("看一下8", err)
-                } else {
-                    console.log("data[0].oid", data[0].oid)
-                    for (let i = 0; i < req.body.newoederList.length; i++) {
-
-
-
-                        DB.query(
-                            olinsql,
-                            [data[0].oid,
-                            req.body.newoederList[i].pid,
-                            req.body.newoederList[i].pname,
-                            req.body.newoederList[i].p_img,
-                            req.body.newoederList[i].fid,
-                            req.body.newoederList[i].fname,
-                            req.body.newoederList[i].f_img,
-                            req.body.newoederList[i].p_quantity,
-                            req.body.newoederList[i].f_quantity,
-                            req.body.newoederList[i].p_os,
-                            req.body.newoederList[i].f_os,
-                            req.body.newoederList[i].p_price,
-                            req.body.newoederList[i].f_price
-                            ],
-                            (err, data) => {
-                                if (err) {
-
-                                    console.log("看一下9", err)
-                                } else {
-                                    console.log("order & list 成功")
-                                    return res.status(200).send("新增成功")
-                                }
-
-                            })
-                    }
+                insql,
+                [
+                    req.body.neworder.mid,
+                    req.body.neworder.order_number,
+                    today,
+                    req.body.neworder.use_date,
+                    req.body.neworder.return_date,
+                    req.body.neworder.price,
+                    req.body.neworder.os,
+                ],
+                (err, data) => {
+                    if (err) reject(err);
+                    else resolve(data);
                 }
-            })
+            );
+        });
+
+        // 步驟2：使用新格式更新order_number
+        const orderId = insertOrderResult.insertId;
+        const newOrderNumber = req.body.neworder.order_number + orderId.toString().padStart(3, '0');
+
+        await new Promise((resolve, reject) => {
+            DB.query(
+                upsql,
+                [newOrderNumber, orderId],
+                (err, data) => {
+                    if (err) reject(err);
+                    else resolve(data);
+                }
+            );
+        });
+
+        // 步驟3：插入訂單清單項目
+        for (let i = 0; i < req.body.newoederList.length; i++) {
+            await new Promise((resolve, reject) => {
+                DB.query(
+                    olinsql,
+                    [
+                        orderId,
+                        req.body.newoederList[i].pid,
+                        req.body.newoederList[i].pname,
+                        req.body.newoederList[i].p_img,
+                        req.body.newoederList[i].fid,
+                        req.body.newoederList[i].fname,
+                        req.body.newoederList[i].f_img,
+                        req.body.newoederList[i].p_quantity,
+                        req.body.newoederList[i].f_quantity,
+                        req.body.newoederList[i].p_os,
+                        req.body.newoederList[i].f_os,
+                        req.body.newoederList[i].p_price,
+                        req.body.newoederList[i].f_price,
+                    ],
+                    (err, data) => {
+                        if (err) reject(err);
+                        else resolve(data);
+                    }
+                );
+            });
         }
 
-    })
+        // 所有查詢都順利完成
+        return res.status(200).send("新增成功");
+    } catch (err) {
+        console.log("錯誤：", err);
+        return res.status(500).send("新增失敗");
+    }
+});
 
     // console.log("看一下js", req.body)
-})
+
 
 module.exports = app;
