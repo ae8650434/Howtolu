feccbf2f49311c471d7354c31f0a544c7cb20c5c
ae8650434 <ae8650434@gmail.com>
2023-07-26 10:19:24 +0800

    首頁功能更新

--- src/components/index/activity.jsx
+++ src/components/index/activity.jsx
@@ -7,9 +7,9 @@ class activity extends Component {
             <div id={actstyle["activity"]}>
             <p>優惠活動～～～</p>
             <ol>
-                <li>到店購買出租設備買一送二，但我們只租不賣</li>
+                <li>到店購買出租設備買一送一</li>
                 <li>凡是去與我們合作的露營地露營，出租設備打75折</li>
-                <li></li>
+              
             </ol>
         </div>
         );
--- src/components/index/weatherapi.jsx
+++ src/components/index/weatherapi.jsx
@@ -1,4 +1,4 @@
-import React, { Component,useState, useEffect } from 'react';
+import React, { Component} from 'react';
 import axios from 'axios';
 import weastyle from '../../css/about.module.css'
 
@@ -10,8 +10,7 @@ class  Weather extends Component {
             records:{
                 locations:[{
                     location:[
-                        {locationName:"",
-                        
+                        {locationName:"",                        
                         weatherElement:[{},
                             {time:[
                                 {elementValue:[{value:null}]
@@ -32,50 +31,108 @@ class  Weather extends Component {
                     ]}
                 ]
             }
-        }
-
+        },
+        date:[{startTime:{}}],
+        sel:0
       } 
+
+     
     render() { 
-        
-        return (<div id="weath">
-        <select id="selectWt" className={weastyle.weasel} onchange="cityChange()">
+       const cnt=2;
+      
+        return (<div id={weastyle["weath"]}>
+        <select id="selectWt" className={weastyle.weasel} onChange={this.cityChange}>
             
             {  
             this.state.data.records.locations[0].location.map((x,index)=>{
-                console.log("44343",x.weatherElement[6].time[0] )
-             return   <option value={index}>{x.locationName}</option>
+                // console.log("44343",x.weatherElement[6].time[0] )
+             return   <option value={index} >{x.locationName}</option>
             })}
               </select>
               
         <div>
             <table id={weastyle["weatb"]}>
                 <tr id={weastyle["thd"]}>
-                    <th>
+                    <th></th>
                     {  
-            this.state.data.records.locations[0].location[0].weatherElement[1].time.map((x,index)=>{
-                // console.log("44343",x.weatherElement[6].time[0].elementValue[0].value )
-                console.log("44343",x )
-          var a = x.startTime
-            
-                return   <th>{a }</th>
+          this.state.date.map((datetime,index)=>{
+                if(index!=0 && index<=7){
+
+                    return   <th>{ datetime}</th>
+                }  
             })}
-                    </th>
+                    
     
                 </tr>
                 <tr id="tmort" className={weastyle.trwea}>
                     <td rowspan="2">早上</td>
+    {
+         this.state.data.records.locations[0].location[this.state.sel].weatherElement[1].time.map((datetime,index)=>{
+            //  console.log(datetime)
+            //  console.log(index)
+             if ( this.state.data.records.locations[0].location[0].weatherElement[1].time.length>14){
+                if(index%2==1){
+                    return   <td>{ datetime.elementValue[0].value}°C</td>
+                }
+
+            } else{
+                if(index%2==0){
+                    return   <td>{ datetime.elementValue[0].value}°C</td>
+                }
+            } 
+        })}
     
                 </tr>
                 <tr id="tmorr">
-    
+                {
+         this.state.data.records.locations[0].location[this.state.sel].weatherElement[6].time.map((datetime,index)=>{
+      
+             if ( this.state.data.records.locations[0].location[0].weatherElement[1].time.length>14){
+                if(index%2==1){
+                    return   <td>{ datetime.elementValue[0].value}</td>
+                }
+
+            } else{
+                if(index%2==0){
+                    return   <td>{ datetime.elementValue[0].value}</td>
+                }
+            } 
+        })}
     
                 </tr>
                 <tr id="taft">
                     <td rowspan="2">下午</td>
+                    {
+         this.state.data.records.locations[0].location[this.state.sel].weatherElement[1].time.map((datetime,index)=>{
+        
+             if ( this.state.data.records.locations[0].location[0].weatherElement[1].time.length>14){
+                if(index!=0 &&index%2==0){
+                    return   <td>{ datetime.elementValue[0].value}°C</td>
+                }
+
+            } else{
+                if(index!=0 &&index%2==1){
+                    return   <td>{ datetime.elementValue[0].value}°C</td>
+                }
+            } 
+        })}
     
                 </tr>
                 <tr id="tafr">
+                {
+         this.state.data.records.locations[0].location[this.state.sel].weatherElement[6].time.map((datetime,index)=>{
     
+             if ( this.state.data.records.locations[0].location[0].weatherElement[1].time.length>14){
+                if(index!=0 &&index%2==0){
+                    return   <td>{ datetime.elementValue[0].value}</td>
+                }
+
+            } else{
+                if(index!=0 &&index%2==1){
+                    return   <td>{ datetime.elementValue[0].value}</td>
+                }
+            } 
+        })}
     
                 </tr>
             </table>
@@ -85,13 +142,41 @@ class  Weather extends Component {
     componentDidMount=async ()=>{
         var result = await axios.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314');
         var newState = { ...this.state };
-        newState = result;
+        newState.data = result.data; 
         
+        
+        var arr= result.data.records.locations[0].location[0].weatherElement[0].time
+
+        if(arr.length%2 !=0){
+            for(var i=1; i<=13 ;i+=2){
+            var a=arr[i].startTime.substr(5,2)+"/"+arr[i].startTime.substr(8,2)
+            newState.date.push(a)
+           
+        }
+        
+        }else{
+            for(var i=0; i<=12 ;i+=2){
+                var a=arr[i].startTime.substr(5,2)+"/"+arr[i].startTime.substr(8,2)
+            newState.date.push(a)
+            
+            // console.log("dadsas",newState.date)
+                // console.log("dadsas",i)
+        }
+      
+    }
+    
+    // console.log("dadsas",newState.date)
+        // console.log("das",newState.date)
         this.state=newState;
         this.setState(this.state);
-        console.log(this.state.data.records.locations[0].location
-            )
-    }
+        // console.log(this.state)
+    } 
+    cityChange=()=>{
+      var city=  document.getElementById("selectWt").selectedIndex
+     
+    //   console.log(this.state)
+      this.setState({sel:city})
+    }  
 }
  
 export default Weather;
