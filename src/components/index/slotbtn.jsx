f9693291e7c03206c7e457cc44c4c9375f5ff652
ae8650434 <ae8650434@gmail.com>
2023-07-28 11:55:40 +0800

    主視覺拉霸機

--- src/components/index/slotbtn.jsx
+++ src/components/index/slotbtn.jsx
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
-import slotstyle from "../../css/slot.module.css"
+import slotstyle from "../../css/slot.css"
 import Slot from "./slot.jsx"
 
 class slotbtn extends Component {
     state = {
-        slotimg: ["camping_1.jpg", "camping_2.jpg", "camping_3.jpg"]
+     
     }
     render() {
         return (
-            <div className={slotstyle.slotdiv}>
-                <Slot />
-            
-               
-            </div>
+            <>
+            <div className='slotoutdiv' >
+                <div id='result' className="slotdiv">
+                        <Slot className='slotimg1' />                
+                        <Slot className='slotimg2' />                    
+                        <Slot className='slotimg3' />        
+                </div>
+                </div>
+                <button onClick={this.slot}>測試用按鈕</button>
+                <button onClick={this.slots}>測試用按鈕</button>
+            </>
         )
     }
-    slot=()=>{
+    slot = () => {
         if (document.getElementById(`result`)) {
             document.getElementById(`result`).classList.add('isplay');
             document.getElementById(`result`).style.transform = 'none';
             console.log(document.getElementById(`result`))
-           
-          }
+
+        }
     }
-    slots=()=>{
+    slots = () => {
         if (document.getElementById(`result`)) {
             const num = Math.floor(Math.random() * 10);
             document.getElementById(`result`).classList.remove('isplay');
-            document.getElementById(`result`).style.transform = `translateY(${-num * 10}%)`;
-          }
+            document.getElementById(`result`).style.transform = `translateY(${ 100}%)`;
+        }
     }
 }
- 
+
 export default slotbtn;
